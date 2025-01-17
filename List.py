dce3de08c0c00479bfc21a1a7e78e8b29e33d60c
onlytoseef <hellotoseef@gmail.com>
2025-01-17 23:34:14 +0500

    new feat

--- List.py
+++ List.py
@@ -25,3 +25,39 @@ tea_varities[1:3]= []
 print(tea_varities)
 # => it will delte that values in list simply it is a list
 
+# for loop in python on list
+
+for tea in tea_varities:
+     print(tea)
+
+# this will print all the items with - sign
+for tea in tea_varities:
+     print(tea,end="-")
+
+# checking if character is present in list or not 
+
+if "yestea" in tea_varities:
+  print("yes i am avaialble")
+
+
+# adding item in last 
+tea_varities.append("last tea")
+print(tea_varities)
+
+# removing last time in list 
+tea_varities.pop()
+# => it will remove item from last
+
+# removing item with specific name
+tea_varities.remove("tea")
+print(tea_varities)
+# adding element to specific index
+tea_varities.insert(0,"tea")
+print(tea_varities)
+
+# creating a deep copy
+
+tea_varities_copy = tea_varities.copy()
+tea_varities_copy.append("last in copy")
+print(tea_varities_copy)
+
